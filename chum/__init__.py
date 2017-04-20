73736534b585b5f351277c5ad8d0af8a2eba3940
Lawrence Wachira <lawrence.wachira@andela.com>
2017-04-20 22:03:32 +0300

    [Chore] Register blueprints -Refactor blueprint names

--- chum/__init__.py
+++ chum/__init__.py
@@ -1,12 +1,8 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 from config import app_configuration
-from .api import api
-
-
-db = SQLAlchemy()
+from .models import db
 
 
 def create_app(config_name):
@@ -19,15 +15,17 @@ def create_app(config_name):
         print("\n\t Invalid configuration key: '{}'. Defaulting...\n".format(
             e.args[0]))
         app.config.from_object(app_configuration[default_config_name])
-
     app.config.from_pyfile('config.py')
 
     db.init_app(app)
 
     migrate = Migrate(app, db)
-
     from . import models
 
-    app.register_blueprint(api, url_prefix='/api/v1')
+    from .api.api import api_bp
+    app.register_blueprint(api_bp, url_prefix='/api/v1')
+
+    from .api.api import auth_api_bp
+    app.register_blueprint(auth_api_bp, url_prefix='/api/v1')
 
     return app
--- chum/api/api.py
+++ chum/api/api.py
@@ -3,10 +3,10 @@ from flask_restful import Api, Resource
 
 
 api_bp = Blueprint('api', __name__)
-auth_bp = Blueprint('auth', __name__)
+auth_api_bp = Blueprint('auth', __name__)
 
 api = Api(api_bp)
-auth = Api(auth_bp)
+auth = Api(auth_api_bp)
 
 
 class UserAPI(Resource):
