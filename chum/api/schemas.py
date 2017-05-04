dcc59a92b5819b4c3d4606881d86b3adfc325a61
Lawrence Wachira <lawrence.wachira@andela.com>
2017-05-04 13:11:20 +0300

    [Feature] Add get_url field methods to obtain resource urls
    -Split UserSchema into Login and Register schemas to handle validations
     separately for each scenario

--- chum/api/schemas.py
+++ chum/api/schemas.py
@@ -1,10 +1,36 @@
-from marshmallow import Schema, fields
+from marshmallow import Schema, fields, validate
 
 
-class UserSchema(Schema):
-    name = fields.String()
-    username = fields.String()
-    email = fields.Email()
+class UserRegisterSchema(Schema):
+    name = fields.String(load_only=True, validate=[validate.Length(max=64)])
+    username = fields.String(
+        required=True,
+        load_only=True,
+        error_messages={
+            'required': 'A username is required to register'}
+    )
+    email = fields.Email(load_only=True, validate=[validate.Length(max=64)])
+    password = fields.String(
+        required=True,
+        load_only=True,
+        validate=[validate.Length(min=6)],
+        error_messages={'required': 'Please send a password'}
+    )
+    password_again = fields.String(
+        required=True,
+        load_only=True,
+        error_messages={'required': 'Please send the password again'})
+
+
+class UserLoginSchema(Schema):
+    username = fields.String(load_only=True)
+    email = fields.Email(load_only=True, validate=[validate.Length(max=64)])
+    password = fields.String(
+        required=True,
+        load_only=True,
+        validate=[validate.Length(min=6)],
+        error_messages={'required': 'Please send a password'}
+    )
 
 
 class BucketListItemSchema(Schema):
@@ -14,10 +40,14 @@ class BucketListItemSchema(Schema):
     date_created = fields.DateTime(dump_only=True)
     date_modified = fields.DateTime(dump_only=True)
     done = fields.Boolean(dump_only=True)
-    uri = fields.Url(dump_only=True)
+    url = fields.Method('get_url', dump_only=True)
 
     class Meta:
         ordered = True
+
+    @staticmethod
+    def get_url(obj):
+        return obj.get_url()
 
 
 class BucketListSchema(Schema):
@@ -27,11 +57,18 @@ class BucketListSchema(Schema):
     date_created = fields.DateTime(dump_only=True)
     date_modified = fields.DateTime(dump_only=True)
     created_by = fields.String(attribute='user.username', dump_only=True)
-    uri = fields.Url(dump_only=True)
+    url = fields.Method('get_url', dump_only=True)
 
     class Meta:
         ordered = True
 
+    @staticmethod
+    def get_url(obj):
+        return obj.get_url()
+
 
 bucketlists_schema = BucketListSchema(exclude=('items',), many=True)
 single_bucketlist_schema = BucketListSchema()
+login_schema = UserLoginSchema()
+register_schema = UserRegisterSchema()
+
