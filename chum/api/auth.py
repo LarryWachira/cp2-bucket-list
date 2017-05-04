a288f3223fe195fd7e1ac78918140303982b4f8a
Lawrence Wachira <lawrence.wachira@andela.com>
2017-05-04 13:20:23 +0300

    [Chore] Remove IGNORE_AUTH configuration setting

--- chum/api/auth.py
+++ chum/api/auth.py
@@ -1,6 +1,7 @@
-from flask import current_app, g, jsonify
+from flask import g
 from flask_httpauth import HTTPTokenAuth
 
+from .utils import error_response
 from ..models import User
 
 
@@ -9,16 +10,11 @@ auth_token = HTTPTokenAuth("Bearer")
 
 @auth_token.verify_token
 def verify_auth_token(token):
-    if current_app.config.get('IGNORE_AUTH') is True:
-        g.user = User.query.get(1)
-    else:
-        g.user = User.verify_auth_token(token)
-    return g.user is not None
+    g.user = User.verify_auth_token(token)
+    return g.user not in ['Invalid token', 'Token expired']
 
 
 @auth_token.error_handler
 def unauthorized_token():
-    response = jsonify({'status': 401, 'error': 'unauthorized',
-                        'message': 'please send your authentication token'})
-    response.status_code = 401
-    return response
+    return error_response(status=401, error='Unauthorized',
+                          message='Please send a valid authentication token')
